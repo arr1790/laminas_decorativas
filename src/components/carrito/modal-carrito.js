56071836fe0860cd1e444c22ad3bc00c00a250fb
arr1790 <anaruano1790@gmail.com>
2025-05-21 21:34:26 +0200

    ...

--- src/components/carrito/modal-carrito.js
+++ src/components/carrito/modal-carrito.js
@@ -4,18 +4,21 @@ import { ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 import { useState, useRef, useEffect } from 'react';
 
-function ModalCarrito( {carrito }) {
+function ModalCarrito({ carrito }) {
+  if (!carrito || !carrito.orderItems) {
+    return <p>El carrito está vacío o no está disponible.</p>;
+  }
 
   console.log(carrito, "- carrito desde modal carrito")
 
-const precioTotal = carrito?.orderItems?.reduce((total, item) => {
-  return total + item.product[0].basePrice * item.cantidad;
-}, 0) || 0;
+  const precioTotal = carrito?.orderItems?.reduce((total, item) => {
+    return total + item.product[0].basePrice * item.cantidad;
+  }, 0) || 0;
 
-const totalItems = carrito?.orderItems?.reduce(
-  (total, item) => total + item.cantidad,
-  0
-) || 0;
+  const totalItems = carrito?.orderItems?.reduce(
+    (total, item) => total + item.cantidad,
+    0
+  ) || 0;
   const [abierto, setAbierto] = useState(false);
   const modalRef = useRef(null);
 
@@ -70,45 +73,45 @@ const totalItems = carrito?.orderItems?.reduce(
                       </svg>
                     </button>
                   </div>
-<ul role="list" className="-my-6 divide-y divide-gray-200">
-                  {/* Productos y contenido */}
-                  <div className="mt-8">
-                    {carrito.orderItems.map((item, index) => (
-                      <li key={item.id + "kai"} className="flex py-6">
-                      <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                        <img src={item.product[0].image} alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="size-full object-cover" />
-                      </div>
-
-                      <div className="ml-4 flex flex-1 flex-col">
-                        <div>
-                          <div className="flex justify-between text-base font-medium text-gray-900">
-                            <h3>
-                              <a href="#">{item.product[0].name}</a>
-                            </h3>
-                            <p className="ml-4">{item.product[0].basePrice}€ </p>
+                  <ul role="list" className="-my-6 divide-y divide-gray-200">
+                    {/* Productos y contenido */}
+                    <div className="mt-8">
+                      {(carrito?.orderItems || []).map((item, index) => (
+                        <li key={item.id + "kai"} className="flex py-6">
+                          <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src={item.product[0].image} alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="size-full object-cover" />
                           </div>
-                          <p className="mt-1 text-sm text-gray-500">{item.product[0].dimensions}</p>
-                          <p className="mt-1 text-sm text-gray-500">{item.product[0].withFrame ? "Con marco" : "Sin marco"}</p>
-                          <p className="mt-1 text-sm text-gray-500">{item.texto1}</p>
-                          <p className="mt-1 text-sm text-gray-500">{item.texto2}</p>
-                        </div>
-                        <div className="flex flex-1 items-end justify-between text-sm">
-                          <form key={item.id + "kei"}  action={restarAlCarrito}> <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Restar</button> <input type="hidden" name='orderItemId' value={item.id} /> </form>
-                          <p className="text-gray-500">{item.cantidad}</p>
-                          <form key={item.id + "keo"} action={sumarAlCarrito}> <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Sumar</button> <input type="hidden" name='orderItemId' value={item.id} /> </form>
 
-                          <div className="flex">
-                            <form key={item.id + "kep"} action={eliminarDelCarrito}>
-                              <input type="hidden" name='orderItemId' value={item.id} />
-                            <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Eliminar macarrones</button>
-                         </form>
-                         </div>
-                        </div>
-                      </div>
-                    </li>
-                    ))}
-                  </div>
-</ul>
+                          <div className="ml-4 flex flex-1 flex-col">
+                            <div>
+                              <div className="flex justify-between text-base font-medium text-gray-900">
+                                <h3>
+                                  <a href="#">{item.product[0].name}</a>
+                                </h3>
+                                <p className="ml-4">{item.product[0].basePrice}€ </p>
+                              </div>
+                              <p className="mt-1 text-sm text-gray-500">{item.product[0].dimensions}</p>
+                              <p className="mt-1 text-sm text-gray-500">{item.product[0].withFrame ? "Con marco" : "Sin marco"}</p>
+                              <p className="mt-1 text-sm text-gray-500">{item.texto1}</p>
+                              <p className="mt-1 text-sm text-gray-500">{item.texto2}</p>
+                            </div>
+                            <div className="flex flex-1 items-end justify-between text-sm">
+                              <form key={item.id + "kei"} action={restarAlCarrito}> <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Restar</button> <input type="hidden" name='orderItemId' value={item.id} /> </form>
+                              <p className="text-gray-500">{item.cantidad}</p>
+                              <form key={item.id + "keo"} action={sumarAlCarrito}> <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Sumar</button> <input type="hidden" name='orderItemId' value={item.id} /> </form>
+
+                              <div className="flex">
+                                <form key={item.id + "kep"} action={eliminarDelCarrito}>
+                                  <input type="hidden" name='orderItemId' value={item.id} />
+                                  <button type="submit" className="font-medium text-indigo-600 hover:text-indigo-500">Eliminar macarrones</button>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </div>
+                  </ul>
                 </div>
 
                 {/* Footer */}
